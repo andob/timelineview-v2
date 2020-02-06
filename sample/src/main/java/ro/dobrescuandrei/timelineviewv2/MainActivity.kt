fefd99784fd2a077ba521d0031915e01a6c848b8
Andrei Dobrescu <andrei.dobrescu@neurony.ro>
2020-02-06 16:18:26 +0200

    project setup

---
+++ sample/src/main/java/ro/dobrescuandrei/timelineviewv2/MainActivity.kt
@@ -0,0 +1,13 @@
+package ro.dobrescuandrei.timelineviewv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
